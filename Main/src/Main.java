5add5017b04acc2ed3b84a99edc915d5da1280d3
Danil Krempolskiy <horolaker@yandex.ru>
2023-03-13 00:06:39 +0300

    first commit

---
+++ Main/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        StepTracker stepTracker = new StepTracker();
+        while (true) {
+            printMenu();
+            int command = scan.nextInt();
+            if (command == 1) {
+                stepTracker.addNewNumberStepsPerDay();
+            } else if (command == 2) {
+                stepTracker.changeStepGoal();
+            } else if (command == 3) {
+                stepTracker.printStatistic();
+            } else if (command == 0) {
+                System.out.println("Выход");
+                return;
+            } else {
+                System.out.println("Нет такой команды");
+            }
+        }
+    }
+    static void printMenu() {
+        System.out.println("Выберите действие:");
+        System.out.println("Ввести количество шагов за определённый день - 1");
+        System.out.println("Изменить цель по количеству шагов в день - 2");
+        System.out.println("Напечатать статистику за определённый месяц - 3");
+        System.out.println("Выйти - 0");
+    }
+}
